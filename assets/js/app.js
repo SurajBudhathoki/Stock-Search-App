4aeb19a46a45b642ba9af33876fcbd63317d8c9d
Suraj Budhathoki <sbudhathoki214@gmail.com>
2018-10-02 19:08:19 -0500

    added links to headlines

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 //creating an array of stocks to display
-const stocksList = ['GOOG','AAPL','AMZN','TSLA','FB'];
+const stocksList = ['GOOG', 'AAPL', 'AMZN', 'TSLA', 'FB'];
 
 //a function to display the required content
-const displayInfo =  function(){
+const displayInfo = function () {
 
     const stock = $(this).attr('data-name');
     const queryURL = `https://api.iextrading.com/1.0/stock/${stock}/batch?types=quote,news,logo&range=1m&last=10`;
@@ -10,16 +10,15 @@ const displayInfo =  function(){
     $.ajax({
         url: queryURL,
         method: 'GET'
-    }).then(function(response){
+    }).then(function (response) {
 
         //storing required information from the api to a variable
         const companyName = response.quote.companyName;
         const stockSymbol = response.quote.symbol;
         const stockPrice = response.quote.latestPrice;
-        
         const logo = response.logo.url;
-    
-      
+
+
         //creating a divBody to hold the information
         const divBody = $('<div>');
         divBody.addClass('card');
@@ -31,15 +30,16 @@ const displayInfo =  function(){
         const symbolHolder = $('<p>').text(`Stock Symbol: ${stockSymbol}`);
         const priceHolder = $('<p>').text(`Stock Price: $${stockPrice}`);
         const additional = $(`<button id ='addinfo'> Details... </button> <br>`)
-    
+
         // Appending the information to our info div in the html
-       
+
         $('#info').append(`<h5 class="card-header">Company Information</h5>`);
-          
-        divContent.append(logoHolder, nameHolder, symbolHolder, priceHolder, additional );
+
+        divContent.append(logoHolder, nameHolder, symbolHolder, priceHolder, additional);
         divBody.append(divContent);
         $('#info').append(divBody);
 
+        //dynamically creating a different card for news information
         const newsBody = $('<div>');
         newsBody.addClass('card');
         const newsContent = $('<div>');
@@ -48,39 +48,34 @@ const displayInfo =  function(){
         const companyNews = response.news;
 
         //looping through the news array to grab the headlines
-        for(let i = 0; i < companyNews.length; i++) {
-         
+        for (let i = 0; i < companyNews.length; i++) {
+
             const headline = response.news[i].headline;
             const url = response.news[i].url;
             newsContent.append(`<p> <a href = "${url}" target="_blank"> ${headline} </a> </p>`);
-          
-        }
-       
 
-        
+        }
 
         $('#news').append(`<h5 class="card-header">Top News</h5>`);
         newsBody.append(newsContent);
         $('#news').append(newsBody);
 
-        const additionalInfo = function(){
+        const additionalInfo = function () {
 
-          
-        
             const queryURL = `https://api.iextrading.com/1.0/stock/${stock}/company`;
-        
+
             $.ajax({
                 url: queryURL,
                 method: 'GET'
-            }).then(function(response){
-        
+            }).then(function (response) {
+
                 const CEO = response.CEO;
                 const desc = response.description;
                 const industry = response.industry;
                 const tags = response.tags;
                 const website = response.website;
-        
-        
+
+
                 const ceoHolder = $('<p>').text(`CEO: ${CEO}`);
                 const descHolder = $('<p>').text(`Description: ${desc}`);
                 const industryHolder = $('<p>').text(`Industry: ${industry}`);
@@ -89,12 +84,12 @@ const displayInfo =  function(){
 
                 divContent.append(ceoHolder, descHolder, industryHolder, tagsHolder, websiteHolder);
             })
-        
+
         }
 
         $('#addinfo').on('click', additionalInfo);
-        
-        
+
+
     });
 
     $('#addinfo').empty();
@@ -107,64 +102,64 @@ const render = function () {
 
     // Deleting the stocks prior to adding new stocks
     $('#buttons').empty();
-  
- 
+
+
     for (let i = 0; i < stocksList.length; i++) {
-  
-    const button = $('<button>');
-      
-    // Adding a class of stock-btn to our button
-    button.addClass('btn btn-outline-secondary stk');
-      
-    // Adding a data-attribute
-    button.attr('data-name', stocksList[i]);
-      
-    button.text(stocksList[i]);
-    
-    $('#buttons').append(button);
+
+        const button = $('<button>');
+
+        // Adding a class of stock-btn to our button
+        button.addClass('btn btn-outline-secondary stk');
+
+        // Adding a data-attribute
+        button.attr('data-name', stocksList[i]);
+
+        button.text(stocksList[i]);
+
+        $('#buttons').append(button);
     }
 }
 
 
 
-const addButton = function(event) {
+const addButton = function (event) {
 
     event.preventDefault();
-    
+
     const validationList = [];
-    const URL =  `https://api.iextrading.com/1.0/ref-data/symbols`;
+    const URL = `https://api.iextrading.com/1.0/ref-data/symbols`;
 
     $.ajax({
-    url: URL,
-    method: 'GET'
-    }).then(function(response){
+        url: URL,
+        method: 'GET'
+    }).then(function (response) {
 
-        for(let i = 0; i < response.length; i++){
+        for (let i = 0; i < response.length; i++) {
 
-        validationList.push(response[i].symbol);
+            validationList.push(response[i].symbol);
 
-        // This line will grab the text from the input box
-        const stock = $('#stockName').val().trim();
-    
-        // validating to make sure only valid stock symbols are accepted
-        if(validationList.includes(stock) && !stocksList.includes(stock)){
-     
-            stocksList.push(stock);
-  
-            // Deletes the contents of the input
-            $('#stockName').val('');
+            // This line will grab the text from the input box
+            const stock = $('#stockName').val().trim();
 
-            render();
-        }  
+            // validating to make sure only valid stock symbols are accepted
+            if (validationList.includes(stock) && !stocksList.includes(stock)) {
 
-        }  
+                stocksList.push(stock);
+
+                // Deletes the contents of the input
+                $('#stockName').val('');
+
+                render();
+            }
+
+        }
 
     })
 
 }
 
 //Event listeners
-$('#add').on('click', addButton);       
+$('#add').on('click', addButton);
 $('#buttons').on('click', '.stk', displayInfo);
 
 render();
